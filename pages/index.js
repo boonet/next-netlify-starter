4ea9698d1019b0010344f00d7d856ffb51115e6a
boonet <ala.naveen.reddy@gmail.com>
2021-12-08 23:47:32 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Name: Swetha Minukuri</title>
+        <title>Name: Swetha Minukuri - Document Validity</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Swetha Minukuri" />
-        <h1 style={{ color: 'green' }}>This document is valid</h1>
-        <Header title="Swetha Minukuri" />
+        <p style={{ color: 'green' }}>This document is valid </p>
+        <p> This is valid document </p>
         <Header title="Swetha Minukuri" />
         <Header title="Swetha Minukuri" />
         <Header title="Swetha Minukuri" />
